5c29545a08cfb6d7a57c4c00a9a14509a0ca5f76
Sabrina Jiang <sabrinajiang@sabrinas-mbp.attlocal.net>
2017-01-10 00:55:10 -0600

    Please enter the commit message for your changes. Lines starting

--- IdeaProjects/RedBlackTree/src/RedBlackTreeTest.java
+++ IdeaProjects/RedBlackTree/src/RedBlackTreeTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
 
 public class RedBlackTreeTest {
     RedBlackTree RBT;
@@ -8,21 +9,31 @@ public class RedBlackTreeTest {
         RBT = new RedBlackTree();
     }
 
-    @Test
+
     public void Insert() {
         RBT.insert(2);
-        System.out.println(RBT.print());
+        System.out.println(RBT);
         RBT.insert(1);
         RBT.findNode(1).setColor(RedBlackTree.Color.BLACK);
-        System.out.println(RBT.print());
+        System.out.println(RBT);
         RBT.insert(3);
         RBT.findNode(3).setColor(RedBlackTree.Color.RED);
-        System.out.println(RBT.print());
+        System.out.println(RBT);
         RBT.insert(4);
-        System.out.println(RBT.print());
+        System.out.println(RBT);
 
         //assertEquals(2, RBT.findNode(1, RBT.root).parent.value);
         //System.out.println(RBT.getUncleColor(RBT.findNode(3, RBT.root)));
     }
-
+    @Test
+    public void Test() {
+        for (int i = 0; i < 10; i++){
+            Random r = new Random();
+            int num = r.nextInt(1000);
+            RBT.insert(num);
+            System.out.print(num + " ");
+        }
+        System.out.println("\n"+RBT);
+        RBT.clear();
+    }
 }
